ab993f6b95b48140e8ac628a200a0254d7ef2bb4
riley1230 <wengjingqi0@gmail.com>
2025-05-01 11:22:56 +0800

    1

---
+++ DOOR ALL/DOOR/data.js
@@ -0,0 +1,335 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "往圖書館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -2.9310846863903457,
+        "pitch": -0.027278298465395423,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6899588684781452,
+          "pitch": 0.10919321946484217,
+          "rotation": 0.7853981633974483,
+          "target": "5--"
+        },
+        {
+          "yaw": -2.1485019549298627,
+          "pitch": -0.024195636285009314,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.835041052459589,
+          "pitch": 0.13496711428591013,
+          "title": "!!!",
+          "text": "早上時間有概率刷出柴犬和陀螺達人"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "常駐捐血車",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -2.3842343045294783,
+        "pitch": 0.0051786766978665355,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.973158153710129,
+          "pitch": 0.0982291934512638,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.7126885119572499,
+          "pitch": 0.12153759661073238,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6119851874751543,
+          "pitch": -0.011153885922217555,
+          "title": "捐血車",
+          "text": "固定時間開啟"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "新生門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 1.603507556381392,
+        "pitch": -0.027639305973879758,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3385403822165678,
+          "pitch": 0.1814081251241113,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 3.1218583263872155,
+          "pitch": 0.08523890783156673,
+          "rotation": 5.497787143782138,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4283638070593483,
+          "pitch": 0.17687503035113572,
+          "title": "新生門",
+          "text": "只有工作人員才可使用"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "館前門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.027221499283282213,
+          "pitch": 0.2684687471977316,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.405508369822483,
+          "pitch": 0.060210520137138346,
+          "title": "館前門",
+          "text": "所有人都可以使用"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "館前門廣場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 2.9903864120557486,
+        "pitch": -0.12722294669842427,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1148363419139535,
+          "pitch": 0.04381035200053063,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.6564187748727122,
+          "pitch": 0.10190813051013414,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7872692846930613,
+          "pitch": 0.055112196268181535,
+          "title": "出沒地點",
+          "text": "有概率刷出廣場舞大媽 柴犬 鴿子"
+        }
+      ]
+    },
+    {
+      "id": "5--",
+      "name": "圖書館 地下入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.969956270828474,
+          "pitch": -0.011284191059470317,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.289318865980281,
+          "pitch": 0.15304589197794805,
+          "rotation": 5.497787143782138,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5394436525390365,
+          "pitch": 0.22396486107658475,
+          "title": "地下室入口",
+          "text": "該地圖尚未開啟"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
